969bc71f1c33702803c8de29881f9d243aab070a
fistvan@scgcontractor.com <fistvan@scgcontractor.com>
2023-03-29 12:00:12 -0400

    can read up to 15

--- lambdas/lambda-1-data-transform.js
+++ lambdas/lambda-1-data-transform.js
@@ -1,31 +1,33 @@
 import fs, { writeFile, writeFileSync } from 'fs';
 import csv from 'csv-parser';
 
-const filePath = 'S3/pie-list.csv';
+//const filePath = `./${process.argv[2]}`;
+const filePath = './S3/banklist-sheet-sorted-15.csv'
+console.log(filePath);
 const outputData = {};
 const outputArray = [];
 
 function dataTransformer() {
 
   return new Promise((resolve, reject) => {
- 
-      fs.createReadStream(filePath)
+
+    fs.createReadStream(filePath)
       .pipe(csv())
       .on('data', (row) => {
         let output = {};
         outputArray.push(row);
-        
+
       })
       .on('end', () => {
         resolve(outputArray);
         console.log("success");
         outputData["someData"] = outputArray;
-        writeFileSync('S3/transformedData.json',JSON.stringify(outputData));
+        writeFileSync('S3/transformedData.json', JSON.stringify(outputData));
 
 
-        
+
       });
-    })
-  }
+  })
+}
 
 export default dataTransformer
--- lambdas/lambda-2-data-update.js
+++ lambdas/lambda-2-data-update.js
@@ -1,6 +1,6 @@
 import { builtinModules } from 'module';
 //import outputData from './lambda-1-data-transform';
-import { promises as fs} from 'fs'; 
+import { promises as fs } from 'fs';
 import jsonfile from 'jsonfile';
 import dataTransformer from './lambda-1-data-transform.js';
 
@@ -10,48 +10,80 @@ async function updateHtml() {
 
     let transform = await dataTransformer();
 
-// Read the contents of the JSON file
+    // Read the contents of the JSON file
     //let read = await jsonfile.readFileSync('./S3/transformedData.json', 'utf8');
-//console.log(read);
-// make sure tyhe JSON is in a format readable in a JS file
-//const jsonData = read;
+    //console.log(read);
+    // make sure tyhe JSON is in a format readable in a JS file
+    //const jsonData = read;
 
-// Convert the JavaScript object to a string with the variable assignment
-//const jsData = `const data = ${JSON.stringify(jsonData)};`;
-      
-// Write the updated contents to the pie-data.js file 
+    // Convert the JavaScript object to a string with the variable assignment
+    //const jsData = `const data = ${JSON.stringify(jsonData)};`;
 
-      //let write = await jsonfile.writeFileSync('pie-data.js', jsData);
+    // Write the updated contents to the pie-data.js file 
+
+    //let write = await jsonfile.writeFileSync('pie-data.js', jsData);
 
 
 
-      const rawData = jsonfile.readFileSync('./S3/transformedData.json');
-      console.log('raw', rawData);
+    const read = jsonfile.readFileSync('./S3/transformedData.json');
+    const rawData = read.someData;
+    console.log("rawData", rawData);
 
-      const stringifiedData = `const data = ${rawData};`;
+    const mapRawData = (rawData) => {
+      const totalCount = rawData.length;
+      const secondColumnCounts = {};
 
-      console.log('stringified', stringifiedData);
-    
-      //jsonfile.writeFileSync('pie-data.js', stringifiedData); 
-      
-      
+      // Count the number of times the second field value appears in the objects
+      for (let i = 0; i < rawData.length; i++) {
+        const value = rawData[i][Object.keys(rawData[i])[1]];
+        secondColumnCounts[value] = secondColumnCounts[value] ? secondColumnCounts[value] + 1 : 1;
+      }
+
+      // Create a new array of objects with the first and second columns
+      const newArray = [];
+      for (let i = 0; i < rawData.length; i++) {
+        const firstColumnValue = rawData[i][Object.keys(rawData[i])[0]];
+        const secondColumnValue = (secondColumnCounts[rawData[i][Object.keys(rawData[i])[1]]] / totalCount) * 100;
+        newArray.push({
+          "name": firstColumnValue,
+          "share": secondColumnValue
+        });
+      }
+
+      return newArray;
+    }
+
+
+    //create a function called rawData parser that takes json input and maps through each key producing a single percentage value as its value
+
+    const mappedData = await mapRawData(rawData);
+
+    console.log("mappedData", mappedData);
+
+    const stringifiedData = `const data = ${mappedData};`;
+
+    console.log('stringified', stringifiedData);
+
+    jsonfile.writeFileSync('pie-data.js', mappedData);
+
+
 
 
   } catch (error) {
     console.error('An error occurred:', error);
 
   }
-// Update the contents of the HTML file
+  // Update the contents of the HTML file
 
-//regex for everything between body tags
+  //regex for everything between body tags
 
-    // const regex = /<script\b[^<]*(?:(?!<\/script >)<[^<]*)*>([\s\S]*?)<\/script>/g;
+  // const regex = /<script\b[^<]*(?:(?!<\/script >)<[^<]*)*>([\s\S]*?)<\/script>/g;
 
-    // let updateHTML = await textData.replace(regex, `<script id="data">${parsedData}</script>`);
+  // let updateHTML = await textData.replace(regex, `<script id="data">${parsedData}</script>`);
 }
 
 
-   
+
 
 // Define the functions to be called
 // async function transform() {
